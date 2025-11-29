0fb8d3493e10cd4d0b2743996e5611d7678bd210
soumyadeep-git <dhalisoumyadeep@gmail.com>
2025-11-29 09:44:39 +0530

    the decision making

---
+++ src/state.py
@@ -0,0 +1,33 @@
+import operator
+from typing import Annotated, List, TypedDict, Optional, Any
+
+class AgentState(TypedDict):
+    """
+    The Single Source of Truth for the Agent's execution.
+    Passed between every node in the LangGraph.
+    """
+    
+    #input
+    dataset_dir: str          
+    
+    #perception
+    file_structure: str       
+    sample_data: str          
+    readme_content: str       
+    
+    #analysis
+    detected_modality: str    
+    detected_task: str        
+    target_column: str        
+    
+    #action
+    plan: str                 
+    python_code: str          
+    
+    #feedback aka the learning stage
+    execution_log: str        
+    success: bool             
+    iterations: int           
+    
+    #logging
+    reasoning_trace: Annotated[List[str], operator.add] 
